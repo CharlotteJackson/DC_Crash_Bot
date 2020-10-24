08131c6690d3c1095c5160c21a23b0f5333500a6
github username <lushiyun@outlook.com>
2020-10-24 19:39:33 -0400

    Add color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   future: {},
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   theme: {
+    colors: {
+      linen: '#EAE2D6',
+      oyster: '#D5C3AA',
+      pewter: '#867666',
+      lemon: '#E1B80D',
+    },
     extend: {},
   },
   variants: {},
