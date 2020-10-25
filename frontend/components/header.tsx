8e08c968579e3d087b6b25107375e3f16c600d8a
github username <lushiyun@outlook.com>
2020-10-25 15:06:46 -0400

    About section done

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -4,7 +4,7 @@ import Container from './container';
 
 const NavItem = ({ href, text }) => (
   <li>
-    <a href={href} className="px-3 py-2 hover:bg-gray-200 rounded">
+    <a href={href} className="text-lg px-3 py-2 hover:bg-gray-200 rounded">
       {text}
     </a>
   </li>
@@ -15,11 +15,11 @@ const Header = () => (
     <Container>
       <nav className="flex justify-between items-center py-8">
         <Link href="/">
-          <a className="text-3xl">
+          <a className="text-4xl">
             walk<span className="text-green-800">safe.</span>
           </a>
         </Link>
-        <ul className="flex space-x-3">
+        <ul className="flex space-x-2">
           <NavItem href="/about" text="about" />
           <NavItem href="/live" text="live" />
           <NavItem href="/history" text="history" />
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,17 @@
 import Head from 'next/head';
 import Container from '../components/container';
 import Layout from '../components/layout';
+import About from '../components/about';
 
 export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>DC Crash Bot</title>
+        <title>Walk Safe DC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container>
-        <h1>Hello World!</h1>
+        <About />
       </Container>
     </Layout>
   );
