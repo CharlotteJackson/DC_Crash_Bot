8d1b253fc76c6fe6ad1b38c6614d45bed54e7a41
Banjo <banjtheman@gmail.com>
2020-11-18 21:09:09 -0500

    can get data from pulsepoint

--- scripts/scrape_pulsepoint.py
+++ scripts/scrape_pulsepoint.py
@@ -1,24 +1,12 @@
-from bs4 import BeautifulSoup
-import requests
-import json
-import uuid
-import datetime
-import boto3
+import pulse
 
 
-
-WEBSITE_URL = "https://web.pulsepoint.org/?agency_id=EMS1205"
+WEBSITE_URL = "https://web.pulsepoint.org/DB/giba.php?agency_id=EMS1205"
 
 def main():
-    print("Scrape now")
 
-    cookies = {"agencies":"EMS1205"}
-    page = requests.get(WEBSITE_URL,cookies=cookies)
-    #need to set cookies
-    
-    html = page.text
-    print(html)
-    soup = BeautifulSoup(html, "html.parser")
+    data = pulse.get_data()
+    print(data)
 
 
 if __name__ == "__main__":
