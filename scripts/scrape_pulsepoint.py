09c60de60da31415febc3ef3b3909003bda35dfc
Banjo <banjtheman@gmail.com>
2020-11-18 21:16:25 -0500

    pulse point data added #20

--- scripts/scrape_pulsepoint.py
+++ scripts/scrape_pulsepoint.py
@@ -1,13 +1,19 @@
+import json
 import pulse
 
 
+# DC pulsepoint website
 WEBSITE_URL = "https://web.pulsepoint.org/DB/giba.php?agency_id=EMS1205"
+
 
 def main():
 
     data = pulse.get_data()
     print(data)
 
+    # # save data to example file
+    # with open("../data/pulsepoint.json", "w") as outfile:
+    #     json.dump(data, outfile)
 
 if __name__ == "__main__":
     main()
