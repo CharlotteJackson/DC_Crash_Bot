8fedc7bf21b208ef19cf3b5efe1ec0701eaf34b7
CharlotteJackson <44366610+CharlotteJackson@users.noreply.github.com>
2020-12-10 00:04:18 -0500

    made scrape pulsepoint for raw jsons its own script

--- scripts/scrape_pulsepoint.py
+++ scripts/scrape_pulsepoint.py
@@ -2,18 +2,37 @@ import json
 import pulse
 
 
-# DC pulsepoint website
-WEBSITE_URL = "https://web.pulsepoint.org/DB/giba.php?agency_id=EMS1205"
+import pandas as pd
+import boto3
+import os
+from pathlib import Path
+from connect_to_rds import get_connection_strings
+import json
+from pulse import get_data
+import datetime
+
+# set up S3 connection
+AWS_Credentials = get_connection_strings("AWS_DEV")
+s3_resource = boto3.resource('s3'
+    ,aws_access_key_id=AWS_Credentials['aws_access_key_id']
+    ,aws_secret_access_key=AWS_Credentials['aws_secret_access_key'])
+bucket_name = AWS_Credentials['s3_bucket']
+region=AWS_Credentials['region']
+prefix = 'source-data/pulsepoint/unparsed/'
+metadata = {'target_schema':'tmp', "dataset_info":"https://docs.google.com/document/pub?id=1qMdahl1E9eE4Rox52bmTA2BliR1ve1rjTYAbhtMeinI#id.q4mai5x52vi6"}
+dataset = 'pulsepoint'
+current_time = datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")
 
 
 def main():
 
     data = pulse.get_data()
-    print(data)
 
-    # # save data to example file
-    # with open("../data/pulsepoint.json", "w") as outfile:
-    #     json.dump(data, outfile)
+    # dump it to file and upload to AWS so we have the raw data for every pull
+    with open('data.json', 'w+') as outfile:
+        json.dump(data, outfile, indent=4)
+    upload = open('data.json', 'rb')
+    s3_resource.Bucket(bucket_name).put_object(Key=prefix+dataset+current_time+'.json', Body=upload, Metadata =metadata)
 
 if __name__ == "__main__":
     main()
