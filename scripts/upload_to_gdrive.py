32d60b393a6ec8b6dd561d7be9030f4700ee1e3c
Banjo <banjtheman@gmail.com>
2021-04-24 20:10:00 -0400

    updated gdrive script to upload json to s3

--- scripts/upload_to_gdrive.py
+++ scripts/upload_to_gdrive.py
@@ -17,6 +17,10 @@ import pandas.io.sql as psql
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+# aws imports
+import boto3
+from botocore.exceptions import ClientError
+
 # Requirments
 # sudo apt-get install libpq-dev python3-dev python3-pip
 # pip install gspread pandas psycopg2 oauth2client
@@ -28,7 +32,7 @@ db_user = os.environ["DB_USER"]
 db_name = os.environ["DB_NAME"]
 
 # Current query we are using
-curr_query = "select reportdate, bicycle_fatalities as fatal_bicyclist, vehicle_fatalities as fatal_driver, pedestrian_fatalities as fatal_pedestrian, total_bicyclists as total_bicycles, total_pedestrians, drivers_impaired as driversimipared, drivers_speeding as speeding_involved, blockkey, 0 fatalpassenger, left(smd_id, 2) anc_id, smd_id, st_X(geography) x_coord, st_Y(geography) y_coord, intersectionid from analysis_data.dc_crashes_w_details where reportdate between date '2015-01-01 00:00:01' and current_date order by reportdate desc"
+curr_query = "select date_trunc('month', reportdate) as reportdate, sum(bicycle_fatalities) as fatal_bicyclist, sum(vehicle_fatalities) as fatal_driver, sum(pedestrian_fatalities) as fatal_pedestrian, sum(total_bicyclists) as total_bicycles, sum(total_pedestrians) as total_pedestrians, blockkey, smd_id, count(*) crash_count , avg(st_X(geography)) x_coord, avg(st_Y(geography)) y_coord from analysis_data.dc_crashes_w_details where reportdate between date '2015-01-01 00:00:01' and current_date group by date_trunc('month', reportdate), blockkey, smd_id order by reportdate desc"
 
 # Needed to upload to google drive
 scope = [
@@ -37,6 +41,10 @@ scope = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive",
 ]
+
+
+# Ask Charlotte for creds
+s3 = boto3.client("s3")
 
 
 def upload_to_goole_sheets(df_loc: str) -> None:
@@ -59,6 +67,54 @@ def upload_to_goole_sheets(df_loc: str) -> None:
         client.import_csv(spreadsheet.id, data=content)
 
 
+def upload_to_goole_sheets(df_loc: str) -> None:
+    """
+    Uploads csv to google drive.
+    Args:
+        - df_loc (str): A filepath to a csv file.
+    Returns:
+        N/A
+    """
+    # must have your custom creds
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        "gdrive_creds.json", scope
+    )
+    client = gspread.authorize(credentials)
+    spreadsheet = client.open("dc_crash_bot")  # Name of spread sheet
+
+    with open(df_loc, "r") as file_obj:
+        content = file_obj.read()
+        client.import_csv(spreadsheet.id, data=content)
+
+
+def upload_file_to_s3(file_name: str, bucket: str, object_name: str = None) -> bool:
+    """
+    Purpose:
+        Uploads a file to s3
+    Args/Requests:
+         file_name: name of file
+         bucket: s3 bucket name
+         object_name: s3 name of object
+    Return:
+        Status: True if uploaded, False if failure
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    try:
+        response = s3.upload_file(
+            file_name, bucket, object_name, ExtraArgs={"ACL": "public-read"}
+        )
+        logging.info(response)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+# TODO cleaup since we dont use google drive anymore
 def main():
     """
     Uploads data to google drive
@@ -75,12 +131,26 @@ def main():
         df = psql.read_sql(curr_query, conn)
 
         # stop gdrive from converting
-        df.update('"' + df["smd_id"].astype(str) + '"')
+        # df.update('"' + df["reportdate"].astype(str) + '"')
+
+        # keep as string
+        df["reportdate"] = df["reportdate"].astype(str)
+
         # save csv
-        df.to_csv("gdrive_test.csv", index=False)
+        #df.to_csv("gdrive_test.csv", index=False)
 
         # upload csv to gdrive
-        upload_to_goole_sheets("gdrive_test.csv")
+        # upload_to_goole_sheets("gdrive_test.csv")
+
+        # Convert df to json
+        df.to_json("gdrive_test.json", orient="records")
+
+        # Upload file to s3
+        upload_file_to_s3(
+            "gdrive_test.json",
+            "dc-crash-bot-public",
+            "source-data/crash_data_for_tableau.json",
+        )
 
     except Exception as error:
         logging.error(error)
