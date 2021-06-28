5e3cc5197c272fe7537828445a8db8dc30773b81
Banjo <banjtheman@gmail.com>
2021-06-28 19:22:14 -0400

    added anc lookup #109

---
+++ ddtsa/data_collectors/get_anc.py
@@ -0,0 +1,97 @@
+# Python imports
+import logging
+import os
+from typing import Tuple, Union, Optional, Dict, Any, List
+
+# 3rd Party Imports
+# from datetime import datetime
+# import datetime
+# from geopy.distance import geodesic
+import pandas as pd
+
+# postgress imports
+import psycopg2
+import pandas.io.sql as psql
+
+# Project Imports
+# TODO what is wrong with imports?
+try:
+    from .get_address import rev_geocode
+except:
+    from get_address import rev_geocode
+
+# Check if google key found
+try:
+    GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+except Exception as error:
+    logging.error("No GOOGLE API KEY detected")
+
+
+# Needed to connect to database
+db_host = os.environ["DB_HOST"]
+db_pass = os.environ["DB_PASS"]
+db_user = os.environ["DB_USER"]
+db_name = os.environ["DB_NAME"]
+
+
+def get_anc_info(address: str, gmap_data: Dict[str, Any] = None) -> str:
+    """
+    Purpose:
+        get 311 requests
+    Args:
+        address: address to check
+        gmap_data: google maps data for address
+    Returns:
+        N/A
+    """
+    conn = psycopg2.connect(
+        f"dbname='{db_name}' user='{db_user}' host='{db_host}' password='{db_pass}'"
+    )
+    logging.info("connected to db")
+
+    # geocode service - using google maps
+    if not gmap_data:
+        gmap_data = rev_geocode(address, GOOGLE_API_KEY)
+
+    # Get the latitude and longitude
+    curr_lat = gmap_data[0]["geometry"]["location"]["lat"]
+    curr_lng = gmap_data[0]["geometry"]["location"]["lng"]
+
+    # Distance
+    DIST = 0  # O miles
+
+    db_query = f"select objectid,anc_id,web_url,name from source_data.anc_boundaries anc WHERE ST_DWithin(ST_MakePoint({curr_lng},{curr_lat}),anc.geography,{DIST}) "
+    df = psql.read_sql(db_query, conn)
+
+    data_json = {}
+    try:
+        data_json["website"] = list(df["web_url"])[0]
+        data_json["name"] = list(df["name"])[0]
+    except:
+        data_json["website"] = ""
+        data_json["name"] = "ERROR NO ANC FOUND"
+
+    return data_json
+
+
+def main():
+    """
+    Purpose:
+        Test the function
+    Args:
+        N/A
+    Returns:
+        N/A
+    """
+    print("hello")
+
+    # Use the test data we have
+    address = "600 Farragut St. NW"
+    print(f"Getting anc info for {address}")
+
+    text = get_anc_info(address)
+    print(text)
+
+
+if __name__ == "__main__":
+    main()
--- ddtsa/ddtsa.py
+++ ddtsa/ddtsa.py
@@ -8,6 +8,7 @@ import logging
 # 3rd Party Imports
 import pandas as pd
 import streamlit as st
+import streamlit.components.v1 as components
 from PIL import Image
 from geopy import distance
 
@@ -19,6 +20,8 @@ from data_collectors.get_prev_311 import get_prev_requests
 from data_collectors.traffic_calming import get_traffic_calming
 from data_collectors.waze_scrapper import get_waze_data
 from data_collectors.get_crash_data import get_safety_concerns
+from data_collectors.get_anc import get_anc_info
+
 
 from data_collectors.get_construction_projects import (
     get_nearby_construction_projects,
@@ -33,94 +36,13 @@ except Exception as error:
     st.error("No GOOGLE API KEY detected")
 
 
-# Full results from Geocoding API for 14th St NW & Columbia Rd NW, Washington, DC 20009
-
-
-def get_open_work_orders(lat, lng: str):
-    """
-    Purpose:
-        Gets open work orders near address
-    Args:
-        address: Current Address
-    Returns:
-        N/A
-    """
-    # Documentation here https://developers.arcgis.com/python/
-    # Hmm can we have the db do geometry for us?
-
-    # TODO we will want to set up the cache from our db
-    # api_url = "https://maps2.dcgis.dc.gov/dcgis/rest/services/DDOT/Cityworks/FeatureServer/2/query?f=json&where=((StatusClosed%20%3D%20%27NOT%20CLOSED%27)%20AND%20(HasWorkOrder%20%3D%20%27YES%27))%20AND%20(datetimeinit%3C%3Dtimestamp%20%272021-04-27%2003%3A59%3A59%27)&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&resultOffset=0&resultRecordCount=500&resultType=standard"
-
-    # TODO we may want to do some preproccessing if this data doesnt change much...
-    # data = requests.get(api_url).json()["features"]
-
-    curr_loc = (lat, lng)
-    work_orders = data_collectors.utils.load_json("work_order.json")
-
-    close_work_orders = []
-
-    # Hmm I dont like work orders, can we get Waze instead?
-    # TODO get waze data
-    # https://developers.google.com/waze/data-feed/incident-information#json-feed-file
-    for work_order in work_orders:
-
-        try:
-
-            # check if is washington dc
-            # District of Columbia
-            for result in work_order["gmap_data"]:
-
-                # TODO is it always 3?
-                if (
-                    not result["address_components"][3]["long_name"]
-                    == "District of Columbia"
-                ):
-                    continue
-
-                work_lat = result["geometry"]["location"]["lat"]
-                work_lng = result["geometry"]["location"]["lng"]
-                # check if address is "near" one of these orders
-                work_order_loc = (work_lat, work_lng)
-
-                # Get current miles
-                curr_dist = distance.distance(curr_loc, work_order_loc).miles
-                # print(curr_dist)
-
-                # TODO What should threshold be?
-                work_order["distance"] = curr_dist
-                if curr_dist < 0.6:
-                    close_work_orders.append(work_order)
-
-                # if curr_dist > 100:
-                #     print(work_order)
-
-                break
-        except Exception as error:
-
-            print(error)
-            print(work_order)
-            continue
-
-    # Hmm do we want to just get xy coords?
-
-    # TODO move this code to a weekly script for new data..
-    # for datum in data:
-
-    #     curr_addres = datum["attributes"]["probaddress"]
-    #     gmap_data = geo_loc_instance.GetGeoLoc(curr_addres)
-    #     print(gmap_data)
-    #     datum["gmap_data"] = gmap_data
-
-    # data_collectors.utils.save_json("work_order.json", data)
-    return close_work_orders
-
-
 def location_data(address: str, gmap_data: Dict[str, Any]):
     """
     Purpose:
         Generates Report
     Args:
         address: Current Address
+        gmap_data: google maps data
     Returns:
         N/A
     """
@@ -149,6 +71,7 @@ def safety_concerns(address: str, gmap_data: Dict[str, Any]):
         Get safety concerns
     Args:
         address: Current Address
+        gmap_data: google maps data
     Returns:
         N/A
     """
@@ -172,6 +95,7 @@ def time_of_day_concerns(address: str, gmap_data: Dict[str, Any]):
     """
     Purpose:
         Get time of day concerns
+        gmap_data: google maps data
     Args:
         address: Current Address
     Returns:
@@ -192,6 +116,7 @@ def existing_traffic_calms(address: str, gmap_data: Dict[str, Any]):
         Get existing traffic calming features
     Args:
         address: Current Address
+        gmap_data: google maps data
     Returns:
         N/A
     """
@@ -210,6 +135,7 @@ def get_neighborhood_uses(address: str):
         Describe neighborhood uses:
     Args:
         address: Current Address
+        gmap_data: google maps data
     Returns:
         N/A
     """
@@ -226,6 +152,7 @@ def get_multi_modal(address: str):
         Get multi‐modal facilities
     Args:
         address: Current Address
+        gmap_data: google maps data
     Returns:
         N/A
     """
@@ -242,6 +169,7 @@ def get_vehicle_types(address: str):
         Get Vehicle types
     Args:
         address: Current Address
+        gmap_data: google maps data
     Returns:
         N/A
     """
@@ -258,6 +186,7 @@ def get_prev_concerns(address: str, gmap_data: Dict[str, Any]):
         Get previous concerns
     Args:
         address: Current Address
+        gmap_data: google maps data
     Returns:
         N/A
     """
@@ -273,6 +202,8 @@ def get_extra_info(address: str):
         Get extra info
     Args:
         address: Current Address
+        gmap_data: google maps data
+
     Returns:
         N/A
     """
@@ -280,6 +211,24 @@ def get_extra_info(address: str):
 
     st.write("We are awesome!!!!!")
 
+def show_anc(address: str, gmap_data: Dict[str, Any]):
+    """
+    Purpose:
+        Get anc info
+    Args:
+        address: Current Address
+        gmap_data: google maps data
+
+    Returns:
+        N/A
+    """
+    data = get_anc_info(address,gmap_data)
+
+    
+    st.subheader(data["name"])
+    st.markdown("**You must have an ANC Commissioner sign off on your TSA**")
+    st.write(f"Here is the ANC website for {address}")
+    st.write(data["website"])
 
 def generate_report(address: str):
     """
@@ -295,6 +244,8 @@ def generate_report(address: str):
     # TODO we should input the google maps data into these functions
     gmap_data = rev_geocode(address, GOOGLE_API_KEY)
     # st.write(gmap_data)
+    # get_anc_info
+    show_anc(address, gmap_data)
 
     location_data(address, gmap_data)
     safety_concerns(address, gmap_data)
